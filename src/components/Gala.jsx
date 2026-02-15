b55bcd47b6d5e85e8456f7b9d60550db78d9afb4
Justinne Baltazar <jfb3@sfu.ca>
2026-02-14 17:03:53 -0800

    update instagram posts and gala responsiveness

--- src/components/Gala.jsx
+++ src/components/Gala.jsx
@@ -2,16 +2,16 @@ export const Gala = () => {
     return (
         <div>
             {/* header */}
-            <div className="w-full flex p-14 justify-center items-center">
-                <div className="w-3/4 md:w-5/8">
-                    <h2 className="text-[#08457E] font-bold text-xl sm:text-2xl md:text-4xl mb-4">Light of Light Gala</h2>
-                    <p className="w-7/8 text-l">Our upcoming annual charity event will focus on advocating for child survival around the world and will feature keynote speaker Sediqa Sarwari, founder of Young Leaders Initiative World. 
+            <div className="w-full flex flex-col sm:flex-row p-14 justify-center items-center">
+                <div className="w-3/4 md:w-5/8 text-center">
+                    <h2 className="text-[#08457E] font-bold text-xl sm:text-2xl md:text-4xl mb-4 text-center sm:text-left">Light of Light Gala</h2>
+                    <p className="w-7/8 text-center sm:text-left text-l mx-auto sm:mx-0">Our upcoming annual charity event will focus on advocating for child survival around the world and will feature keynote speaker Sediqa Sarwari, founder of Young Leaders Initiative World. 
                         We’ll also have food, a photo booth, and tons of chances to win raffle prizes!</p>
-                    <div className="flex gap-2 my-4">
-                        <div className="bg-[#08457E] text-sm sm:text-md md:text-lg w-2/5 sm:w-2/7 md:w-2/7 lg:w-1/5 text-[#EBFAFF] rounded-xl p-2 text-center">
+                    <div className="flex flex-col sm:flex-row gap-2 my-4">
+                        <div className="bg-[#08457E] text-sm sm:text-md md:text-lg w-full sm:w-2/7 md:w-2/7 lg:w-1/5 text-[#EBFAFF] rounded-xl p-2 text-center">
                             SFU Burnaby
                         </div>
-                        <div className="bg-[#EBFAFF] text-sm sm:text-md md:text-lg w-3/5 sm:w-3/7 md:w-3/7 lg:w-1/3 text-[#08457E] rounded-xl p-2 text-center">
+                        <div className="bg-[#EBFAFF] text-sm sm:text-md md:text-lg w-full sm:w-3/7 md:w-3/7 lg:w-1/3 text-[#08457E] rounded-xl p-2 text-center">
                             March 20th at 5:30 PM
                         </div>
                     </div>
@@ -24,7 +24,7 @@ export const Gala = () => {
 
             {/* tickets banner */}
             <div className="bg-[#EBFAFF] w-full flex flex-col p-15 lg:p-20 justify-center items-center gap-5 sm:gap-8">
-                <h2 className="text-[#08457E] text-xl sm:text-2xl lg:text-4xl font-bold">Early Bird Tickets Available Now!</h2>
+                <h2 className="text-[#08457E] text-center text-xl sm:text-2xl lg:text-4xl font-bold">Early Bird Tickets Available Now!</h2>
                 <p className="text-l sm:text-xl text-center w-4/5">Join us for an inspiring evening of advocacy, entertainment, and networking, all while supporting an important cause!</p>
                 <div className="bg-[#08457E] text-white text-xl rounded-xl p-3 md:p-4 text-center">
                     <a href="https://www.bouncelife.com/events/6988266b316bad8542535c2e"
@@ -34,7 +34,7 @@ export const Gala = () => {
 
             {/* guest speaker */}
             <div className="w-full flex flex-col p-15 justify-center items-center gap-6 mb-7">
-                <h2 className="text-[#08457E] font-bold text-2xl md:text-3xl">Our Keynote Speaker</h2>
+                <h2 className="text-[#08457E] text-center font-bold text-2xl md:text-3xl">Our Keynote Speaker</h2>
                 <p className="text-center text-l md:text-xl w-3/4">Meet our guest speaker for the Light of Light Gala: Sediqa Sarwari. Don’t miss this opportunity to hear from an inspiring speaker! Get your tickets now!</p>
                 
                 <div className="w-full flex flex-col md:flex-row justify-center items-center gap-12 mt-12">
